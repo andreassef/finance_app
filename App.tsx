19aa247962802f5b365a2bd8f87f26ca7d7c6b12
Andre Assef <andreassef02@gmail.com>
2021-11-18 00:26:17 -0300

    [ UPDATE ] - Ajustes no arquivo de entrada

--- App.tsx
+++ App.tsx
@@ -1,22 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Welcome from './src/Welcome';
+import AppLoading from 'expo-app-loading';
+import theme from './src/global/styles/theme';
+
+import { ThemeProvider } from 'styled-components';
+import { Dashboard } from './src/screens/Dashboard';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
 
 export default function App() {
+
+  const [ fontsLoaded ] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Welcome title="VASCOOO" />
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

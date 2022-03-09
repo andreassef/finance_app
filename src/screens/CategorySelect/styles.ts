d0a0f17635f1053adb2dca3ac770ab3103687e6f
Andre Assef <andreassef02@gmail.com>
2022-03-09 00:03:02 -0300

    [ UPDATE ] - Utilizando gesturehandler

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
 
-export const Container = styled.View`
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export const Container = styled(GestureHandlerRootView)`
     flex: 1;
     background-color: ${({ theme }) => theme.colors.background};
 `;

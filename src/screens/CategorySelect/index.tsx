b1cbf80217d42f9325e1d4d7de9691d2c41e87be
Andre Assef <andreassef02@gmail.com>
2022-03-09 00:37:41 -0300

    [ UPDATE ] - Nova funcao e novas propriedades nos componentes

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -10,7 +10,7 @@ interface Category {
 }
 
 interface Props {
-    category: string;
+    category: Category;
     setCategory: (category: Category) => void;
     closeSelectCategory: () => void;
 }
@@ -20,6 +20,11 @@ export function CategorySelect({
     setCategory,
     closeSelectCategory
 }: Props){
+
+    function handleCategorySelect(category: Category) {
+        setCategory(category);
+    }
+
     return(
         <Container>
             <Header>
@@ -31,7 +36,10 @@ export function CategorySelect({
                 style={{ flex: 1, width: '100%'}}
                 keyExtractor={(item) => item.key}
                 renderItem={({ item }) => (
-                    <Category>
+                    <Category
+                        onPress={() => handleCategorySelect(item)}
+                        isActive={category.key === item.key}
+                    >
                         <Icon name={item.icon} />
                         <Name>{item.name}</Name>
                     </Category>
@@ -39,8 +47,10 @@ export function CategorySelect({
                 ItemSeparatorComponent={()=> < Separator />}
             />
             <Footer>
-                <Button title='Selecionar' />
-                
+                <Button 
+                    title='Selecionar'
+                    onPress={closeSelectCategory} 
+                />
             </Footer>
         </Container>
     )

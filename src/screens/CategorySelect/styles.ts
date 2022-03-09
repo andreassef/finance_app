d09ac1e983623b71ceaf5f830c45d43d826184cc
Andre Assef <andreassef02@gmail.com>
2022-03-09 00:38:28 -0300

    [ UPDATE ] - Adicao de nova interface e novo estilo ao selecionar o conteudo na lista

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -3,6 +3,11 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
 
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import theme from '../../global/styles/theme';
+
+interface CategoryProps {
+    isActive: boolean;
+}
 
 export const Container = styled(GestureHandlerRootView)`
     flex: 1;
@@ -18,11 +23,15 @@ export const Header = styled.View`
     padding-bottom: 19px;
 `;
 
-export const Category = styled.View`
+export const Category = styled.TouchableOpacity<CategoryProps>`
     width: 100%;
     padding: ${RFValue(15)}px;
     flex-direction: row;
     align-items: center;
+
+    background-color: ${({ isActive }) => 
+        isActive ? theme.colors.secondary_light : theme.colors.background
+    }
 `;
 
 export const Icon = styled(Feather)`
